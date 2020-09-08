06d44f4804bed2b841411d6af0ffb1d5e6f345cb
Rodolphe <dutchr@live.fr>
2020-09-08 16:37:51 +0200

    Finish input form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'reddit-clone';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(
+      `Ajouter article titre:${title.value} et le lien :${link.value}`
+    );
+    return false;
+  }
 }
