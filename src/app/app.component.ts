a18d8bd0be690637bf2a5745b1911db5ec55f5e1
Rodolphe <dutchr@live.fr>
2020-09-09 10:30:12 +0200

    Finish displaying articles into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './components/article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  articles: Article[]
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(
       `Ajouter article titre:${title.value} et le lien :${link.value}`
     );
     return false;
   }
-}
+ constructor() {
+   this.articles = [
+     new Article('Angular','http://angular.io',3),
+     new Article('React','http://react.com', 2),
+     new Article('VueJS', 'http://vuejs.com',1),
+   ];
+ }
+ }
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,30 +1,26 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-@HostBinding('attr.class') cssClass = 'row';
-votes: number;
-title:string;
-link:string; 
+  @Input() article: Article;
+  @HostBinding('attr.class') cssClass = 'row';
+  
   constructor() {
-    this.title = 'Angular';
-    this.link = 'http://angular.io';
-    this.votes = 10
-   }
-   voteUp() :boolean {
-     this.votes += 1;
-     return false
-   }
-   voteDown() :boolean {
-     this.votes -= 1;
-     return false
-   }
-
-  ngOnInit(): void {
+    this.article = new Article('Angular', 'http://angular.io', 10);
+  }
+  voteUp(): boolean {
+    this.article.votes += 1;
+    return false;
+  }
+  voteDown(): boolean {
+    this.article.votes -= 1;
+    return false;
   }
 
+  ngOnInit(): void {}
 }
